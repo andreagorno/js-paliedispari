9b8e70d2d0607daf83c6d5371495213937650370
andreagorno <andreagorno98@gmail.com>
2021-05-03 17:50:19 +0200

    esercizio corretto

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -8,15 +8,10 @@
 // utente sceglie pari o dispari
 var firstChoice = prompt("Scegli tra pari o dispari");
 
-while (firstChoice != "pari" || firstChoice != "dispari") {
+while (firstChoice != "pari" && firstChoice != "dispari") {
     firstChoice = prompt("Selezione sbagliata, per favore inserisci una a scelta tra pari o dispari");
-    console.log(firstChoice);
 }
 console.log("Hai scelto " + firstChoice);
-// if (firstChoice != "pari" || firstChoice != "dispari") {
-//     alert("Inserimento errato, per favore riprovare");
-//     firstChoice = prompt("Scegli tra pari o dispari");
-// }
 
 // utente inserisce un numero da 1 a 5
 var secondChoice = parseInt(prompt("Inserisci un numero da 1 a 5"));
@@ -24,20 +19,8 @@ var secondChoice = parseInt(prompt("Inserisci un numero da 1 a 5"));
 while (secondChoice > 5 || secondChoice < 1) {
     secondChoice = parseInt(prompt("Selezione sbagliata, per favore inserisci un numero compreso tra 1 e 5"));
 
-    secondChoice >= 5 || secondChoice <= 1;
 }
 console.log("Hai scelto il numero " + secondChoice);
-// if (secondChoice < 1 || secondChoice > 5) {
-//     alert("Inserimento errato, per favore riprovare");
-//     secondChoice = parseInt(prompt("Inserisci un numero da 1 a 5"));
-// } else if (isNaN(secondChoice)) {
-//     alert("Inserimento errato, per favore riprovare");
-//     secondChoice = parseInt(prompt("Inserisci un numero da 1 a 5"));
-// } else if (secondChoice == "") {
-//     alert("Inserimento errato, per favore riprovare");
-//     secondChoice = parseInt(prompt("Inserisci un numero da 1 a 5"));
-// }
-// console.log("Hai inserito il numero " + secondChoice);
 
 // generare un numero random
 function generateNumber(min, max) {
